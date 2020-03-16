de710b28d6fae30a9cc0b2674b6741a186c0fe17
Wahyuni <61979599+13020180040@users.noreply.github.com>
2020-03-16 23:13:55 +0800

    Add files via upload

---
+++ ayu/tugas.java
@@ -0,0 +1,19 @@
+/*	Nama 		: wahyuni
+	kelas		: B1
+	Wktu 		:21.13
+	hari/tanggal	:senin/16 maret 2020
+*/
+public class tugas {
+	public static void main(String[] args){
+	String nim 	="13020180040";
+	String nama	= "Wahyuni";
+	String jurusan	= "Teknik Informatika";
+	String fakultas	= "Ilmu Komputer";
+	System.out.println("nim saya : "+ nim);
+	System.out.println("nama saya : "+ nama);
+	System.out.println("jurusan saya : "+ jurusan);
+	System.out.println("fakultas saya : "+ fakultas);
+}
+}
+	 
+	 
